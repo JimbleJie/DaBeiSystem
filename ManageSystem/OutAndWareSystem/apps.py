1b05fcdad3f913fd372ea67010b7db0e726fee8c
yanjj.vendor <yanjj.vendor@tetras.ai>
2026-06-30 11:32:24 +0800

    删掉工程

--- ManageSystem/OutAndWareSystem/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class OutandwaresystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'OutAndWareSystem'
-    verbose_name = '出入库管理系统'
