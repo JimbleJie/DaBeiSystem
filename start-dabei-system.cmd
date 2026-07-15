a1c5dd287423fc47d2a2fd274b4dc94e7d165fe8
yanjj.vendor <yanjj.vendor@tetras.ai>
2026-07-15 14:32:00 +0800

    Fix Windows launcher command encoding

--- start-dabei-system.cmd
+++ start-dabei-system.cmd
@@ -8,7 +8,7 @@ set "LOG_DIR=E:\DaBeiSystem-main\DaBeiSystem-main\log"
 cd /d "%PROJECT_DIR%" || goto failed
 if not exist "%LOG_DIR%" mkdir "%LOG_DIR%"
 
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%~dp0start-all-windows.ps1" -ProjectDir "%PROJECT_DIR%" -NodeDir "%NODE_DIR%" -LogDir "%LOG_DIR%" %*
+powershell.exe -NoProfile -ExecutionPolicy Bypass -File "%PROJECT_DIR%\scripts\start-all-windows.ps1" -ProjectDir "%PROJECT_DIR%" -NodeDir "%NODE_DIR%" -LogDir "%LOG_DIR%"
 
 if errorlevel 1 goto failed
 goto done
